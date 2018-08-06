20a02299b5021db55f51414c0de330edf706c41e
Omar Aflak <aflakomar@gmail.com>
2018-08-06 11:45:46 +0200

    renamed parser.h parser.cpp

--- root/include/parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER
-#define PARSER
-
-#include <string>
-
-bool hasArg(const std::string& arg, int argc, char const *argv[]);
-std::string getArg(const std::string& arg, int argc, char const *argv[]);
-
-#endif
